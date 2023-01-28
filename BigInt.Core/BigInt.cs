553c69225caa6ccfe4812b1d5613ce32d8296517
sh1ngekyo <repllimate@gmail.com>
2023-01-28 22:36:59 +0300

    Add basic ctors & operators

--- BigInt.Core/BigInt.cs
+++ BigInt.Core/BigInt.cs
@@ -1,7 +1,83 @@
-﻿namespace BigInt.Core
-{
-    public class BigInt
-    {
+﻿using BigInt.Core.Models;
 
+using System;
+
+namespace BigInt.Core
+{
+    public sealed class BigInt
+    {
+        private Data Data { get; set; }
+
+        public BigInt(long source)
+        {
+            CreateFromLong(source);
+        }
+
+        public BigInt(int source)
+        {
+            CreateFromInt(source);
+        }
+
+        public BigInt(uint source)
+        {
+            CreateFromUInt(source);
+        }
+
+        public BigInt(ulong source)
+        {
+            CreateFromULong(source);
+        }
+
+        private void CreateFromNumber(ulong source, bool signed)
+        {
+            var bits = source.ToString().ToCharArray().Select(c => (byte)c).Reverse().ToArray();
+            Data = new Data(bits, bits.Length, signed);
+        }
+
+        private void CreateFromInt(int source)
+        {
+            var signed = source < 0;
+            if (signed)
+                source *= -1;
+            CreateFromNumber((ulong)source, signed);
+        }
+
+        private void CreateFromLong(long source)
+        {
+            var signed = source < 0;
+            if (signed)
+                source *= -1;
+            CreateFromNumber((ulong)source, signed);
+        }
+
+        private void CreateFromUInt(uint source)
+        {
+            CreateFromNumber(source, false);
+        }
+
+        private void CreateFromULong(ulong source)
+        {
+            CreateFromNumber(source, false);
+        }
+
+        public static implicit operator BigInt(ulong data)
+        {
+            return new BigInt(data);
+        }
+
+        public static implicit operator BigInt(long data)
+        {
+            return new BigInt(data);
+        }
+
+        public static implicit operator BigInt(uint data)
+        {
+            return new BigInt(data);
+        }
+
+        public static implicit operator BigInt(int data)
+        {
+            return new BigInt(data);
+        }
     }
 }
---
+++ BigInt.Core/Models/Data.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigInt.Core.Models
+{
+    internal class Data
+    {
+        internal byte[] Bits { get; set; }
+        internal int Size { get; set; }
+        internal bool Signed { get; set; }
+
+        internal Data(byte[] bits, int size, bool signed)
+        {
+            Bits = bits;
+            Size = size;
+            Signed = signed;
+        }
+    }
+}
